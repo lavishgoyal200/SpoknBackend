09312b9be594a2c070e3d4bba1aee58cf24902ce
Lavish <goyallavish200@gmail.com>
2025-06-30 17:35:23 +0530

    link update

--- src/server.js
+++ src/server.js
@@ -22,7 +22,9 @@ app.use(cors({
     origin: [
         "http://localhost:5173", 
         "https://spokn1.vercel.app", 
-        "https://spokn-lp4yu6i6n-lavishs-projects-36ec3d3b.vercel.app"
+        "https://spokn-c7gm6hf7p-lavishs-projects-36ec3d3b.vercel.app",
+        "https://spokn-lavishs-projects-36ec3d3b.vercel.app",
+
     ],
     credentials: true
 }));
