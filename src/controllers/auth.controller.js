c6ff74741c2556dbd5e6c070e348d0997a3c4051
Lavish <goyallavish200@gmail.com>
2025-06-30 15:37:44 +0530

    SameSite fixed

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -51,8 +51,8 @@ export async function signup(req,res) {
         res.cookie("jwt",token,{
             maxAge: 7*24*60*60*1000,
             httpOnly:true, // prevent XSS attacks,
-            // sameSite:"None",  // prevent CSRF attacks
-             sameSite:"strict",  // prevent CSRF attacks
+            sameSite:"None",  // prevent CSRF attacks
+            //  sameSite:"strict",  // prevent CSRF attacks
             secure: process.env.NODE_ENV === "production"
         })
 
@@ -86,8 +86,8 @@ export async function login(req,res){
         res.cookie("jwt",token,{
             maxAge: 7*24*60*60*1000,
             httpOnly:true, // prevent XSS attacks,
-            // sameSite:"None", // prevent CSRF attacks
-             sameSite:"strict",  // prevent CSRF attacks
+            sameSite:"None", // prevent CSRF attacks
+            //  sameSite:"strict",  // prevent CSRF attacks
             secure: process.env.NODE_ENV === "production"
         })
 
